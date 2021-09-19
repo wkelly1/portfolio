e3f0f8f953a72b66f69cda18fc420ef16d9a20d0
Will Kelly <william.kelly20@btinternet.com>
2021-09-19 23:36:44 +0100

    Fixing link

--- projects.config.js
+++ projects.config.js
@@ -9,7 +9,7 @@ const projects = [
     title: "IKONO",
     description: "SVG Icon pack",
     languages: ["JavaScript", "React"],
-    link: "https://github.com/wkelly1/Valigators",
+    link: "ikono.will-kelly.co.uk",
   },
   {
     title: "Carousel",
